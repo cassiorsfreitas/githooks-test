0d1d1ba74fe04b218aa958f5590460094bb3e7f6
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-04-02 13:48:19 +0100

    Testing useEffects, fetch repos and handleFavorites

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect( async () => {
+    const response = await fetch("https://api.github.com/users/cassiorsfreitas/repos")
+    const data = await response.json()
+
+    setRepositories(data)
+  }, [])
+
+  useEffect( () => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `You have ${filtered.length} favorites`
+  })
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map( repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {repositories.map( repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>Added</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Bookmarks</button>
+          </li>
+      ))}
+    </ul>
   );
 }
 
